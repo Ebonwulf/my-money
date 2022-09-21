5979331a60c0bb6a1ed26dfa4f73074b80949d98
ebonwulf <ebonwolf00@gmail.com>
2022-09-21 13:32:32 +0100

    form element added to the signup component with all inputs required. state created and implemented for the form submition and handleSubmit function created

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -14,29 +14,27 @@ const Login = () => {
     /*the [''] after styles (instead of the .) for the class name are required if your class name has a - in it because js thinks it is a subtraction not a hyphen*/
   }
   return (
-    <>
-      <form onSubmit={handleSubmit} className={styles['login-form']}>
-        <h2>Login</h2>
-        <label>
-          <span>Email:</span>
-          <input
-            type='email'
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </label>
-        <label>
-          <span>Password:</span>
-          <input
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-          />
-        </label>
-        {/*we don't need the styles.classname for the buttom because they styles are in the index.scss file, which is not a module file */}
-        <button className='btn'>Login</button>
-      </form>
-    </>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      {/*we don't need the styles.classname for the buttom because they styles are in the index.scss file, which is not a module file */}
+      <button className='btn'>Login</button>
+    </form>
   );
 };
 
--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
 import styles from './Signup.module.scss';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Username:</span>
+        <input
+          type='text'
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className='btn'>Sign up</button>
+    </form>
+  );
 };
 
 export default Signup;
