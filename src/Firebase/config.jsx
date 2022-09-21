8c254b65635d974c56f3d656498716167cb5d4e6
ebonwulf <ebonwolf00@gmail.com>
2022-09-21 13:41:24 +0100

    firebase installed and a config.jsx created for all the config settings and initialzation of the firebase and the service

---
+++ src/Firebase/config.jsx
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCnI2gswrgLMoZHW0zYMB_Ph8M5xq-o5tw',
+  authDomain: 'mymoney-cc256.firebaseapp.com',
+  projectId: 'mymoney-cc256',
+  storageBucket: 'mymoney-cc256.appspot.com',
+  messagingSenderId: '44043871968',
+  appId: '1:44043871968:web:4c6d41b54b76b0966b9285',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
