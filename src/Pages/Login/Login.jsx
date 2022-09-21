1b880b5fa9e7b7959c0dbba5af46c53c9fad0361
ebonwulf <ebonwolf00@gmail.com>
2022-09-21 13:05:34 +0100

    form added to the login component and styles added. Basic submit functionality added to the form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,43 @@
+import { useState } from 'react';
 import styles from './Login.module.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  {
+    /*the [''] after styles (instead of the .) for the class name are required if your class name has a - in it because js thinks it is a subtraction not a hyphen*/
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={styles['login-form']}>
+        <h2>Login</h2>
+        <label>
+          <span>Email:</span>
+          <input
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        {/*we don't need the styles.classname for the buttom because they styles are in the index.scss file, which is not a module file */}
+        <button className='btn'>Login</button>
+      </form>
+    </>
+  );
 };
 
 export default Login;
