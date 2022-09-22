6eaabfb5bc5dd387ffac773bfaf0e251e70bc1d1
ebonwulf <ebonwolf00@gmail.com>
2022-09-22 09:44:37 +0100

    added a login case to the switch in the AuthContext and implemented it in the useSignup hook

--- src/Hooks/useSignup.jsx
+++ src/Hooks/useSignup.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { projectAuth } from '../Firebase/config';
+import { useAuthContext } from '../Hooks/useAuthContext';
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   //parameters for signup come from the form items in signup.jsx
   const signup = async (displayName, email, password) => {
@@ -15,13 +17,13 @@ export const useSignup = () => {
         email,
         password
       );
-      console.log(res.user);
       if (!res) {
         throw new Error('Could not complete sign up');
       }
-
       //add display name to user
       await res.user.updateProfile({ displayName: displayName });
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user });
       setIsPending(false);
       setError(null);
     } catch (err) {
