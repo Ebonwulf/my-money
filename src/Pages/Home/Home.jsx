570a810d6a7ac4b768d8e27434ca3c874d71ee71
ebonwulf <ebonwolf00@gmail.com>
2022-09-27 10:56:25 +0100

    useFirestore hook created to handle the adding and deleting of transactions. function for adding a document to the firestore completed and the switch cases written for this and the isPending state

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
+import TransactionForm from '../../Components/TransactionForm/TransactionForm';
 import styles from './Home.module.scss';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction List</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
