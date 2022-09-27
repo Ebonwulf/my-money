27df936c749ef9c1e3b57a3f84eff19f60ea0870
ebonwulf <ebonwolf00@gmail.com>
2022-09-27 09:59:54 +0100

    authContext.jsx updated so that firebase is checked for a logged in user when the page is refreshed, so that you do not breifly get the login and signup links on the navbar. Redirect implemented in the app component so that pages are protected based on whether there is a user logged in or not and redirects them to the appropriate page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { useAuthContext } from './Hooks/useAuthContext';
 import NavBar from './Components/NavBar/NavBar';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Signup from './Pages/Signup/Signup';
 
 const App = () => {
+  const { authIsReady, user } = useAuthContext();
   return (
     <div className='App'>
-      <BrowserRouter>
-        <NavBar />
-        {/*The switch is needed around the routes to prevent them all showing on the page at once */}
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/login'>
-            <Login />
-          </Route>
-          <Route path='/signup'>
-            <Signup />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <NavBar />
+          {/*The switch is needed around the routes to prevent them all showing on the page at once */}
+          <Switch>
+            {/*using Redirect protects pages from being accessed based on whether there is a logged in user or not and redirects then to the appropriate page */}
+            <Route exact path='/'>
+              {!user && <Redirect to='/login' />}
+              {user && <Home />}
+            </Route>
+            <Route path='/login'>
+              {user && <Redirect to='/' />}
+              {!user && <Login />}
+            </Route>
+            <Route path='/signup'>
+              {user && <Redirect to='/' />}
+              {!user && <Signup />}
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      )}
     </div>
   );
 };
--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
+import { projectAuth } from '../Firebase/config';
 
 export const AuthContext = createContext();
 
@@ -8,13 +9,28 @@ export const authReducer = (state, action) => {
       return { ...state, user: action.payload };
     case 'LOGOUT':
       return { ...state, user: null };
+    case 'AUTH_IS_READY':
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
 };
 
 export const AuthContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+    authIsReady: false,
+  });
+
+  useEffect(() => {
+    //this function communicates with firebase to get firebase to tell it whenever there is a change in the authentication status, and when there is it fires the function inside it. It is also fires the function when we first communicate with firebase to check for a user to begin with, when we first reload the page.
+    const unsub = projectAuth.onAuthStateChanged((user) => {
+      dispatch({ type: 'AUTH_IS_READY', payload: user });
+      //by storing the above function as a const we can then call it after the ispatch so that it will stop the function from being fired again
+      unsub();
+    });
+  }, []);
+
   console.log('AuthContext state: ', state);
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
